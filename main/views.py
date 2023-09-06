4d3362893c04f3b832ff03e61273bd191d3deed7
yosef <yosefnuragaw@gmail.com>
2023-09-06 15:25:15 +0700

    Fix name and class implementation

--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP A'
+        'name': 'Yosef Nuraga Wicaksana',
+        'class': 'PBP C'
     }
 
     return render(request, "main.html", context)
